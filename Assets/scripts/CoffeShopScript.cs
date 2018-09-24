16b32406151af483c71d905abe78f22ae36b2236
Nishchint <nishchint34@gmail.com>
2018-09-24 16:50:24 -0700

    Updating test world, sphere with keyboard movement

---
+++ Assets/scripts/CoffeShopScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class CoffeShopScript : MonoBehaviour {
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Entering new scene");
+        SceneManager.LoadScene("Newcoffeescene");
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	}
+
